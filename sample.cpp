2d2755065ea34c1928162e81c70740a765a86cae
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-10 19:04:46 +0530

    Program to demonstrate a sample program in cpp

---
+++ sample.cpp
@@ -0,0 +1,18 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate a sample program in cpp.
+Author  : Chpaone09®
+Date    : Oct 10, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+   cout<<"\n\n\t Hello World";
+   return 0;
+}
